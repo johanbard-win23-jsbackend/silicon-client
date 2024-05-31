2129cfaf10874ba9c802daac9cc7b79afc4da92b
daBard <50872455+daBard@users.noreply.github.com>
2024-05-31 18:50:41 +0200

    added output: 'standalone'

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
-    },
+   output: 'standalone'
 };
+
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//           {
+//             protocol: "https",
+//             hostname: "**",
+//           },
+//         ],
+//     },
+// };
 
 export default nextConfig;
