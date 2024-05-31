3a28d29fc9b8d01ebf029f2310b2aa0d45c42b7c
daBard <bard.johan@gmail.com>
2024-05-31 16:47:10 +0200

    bugfix

--- app/context/GlobalContext.tsx
+++ app/context/GlobalContext.tsx
@@ -9,19 +9,19 @@ interface GlobalContextProps {
 const GlobalContext = createContext<GlobalContextProps | undefined>(undefined);
 
 export const GlobalProvider = ({ children }: { children: ReactNode }) => {
-  const [profileImg, setProfileImg] = useState<string>('');
+  //const [profileImg, setProfileImg] = useState<string>('');
 
   return (
-    <GlobalContext.Provider value={{ profileImg, setProfileImg }}>
+    //<GlobalContext.Provider value={{ profileImg, setProfileImg }}>
       {children}
-    </GlobalContext.Provider>
+    //</GlobalContext.Provider>
   );
 };
 
-export const useGlobalContext = (): GlobalContextProps => {
-  const context = useContext(GlobalContext);
-  if (!context) {
-    throw new Error('useGlobalContext must be used within a GlobalProvider');
-  }
-  return context;
-};
+// export const useGlobalContext = (): GlobalContextProps => {
+//   const context = useContext(GlobalContext);
+//   if (!context) {
+//     throw new Error('useGlobalContext must be used within a GlobalProvider');
+//   }
+//   return context;
+// };
